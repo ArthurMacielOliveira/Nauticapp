e4b49f07f6141a10b2373ab4dfc6668e58ec6a6e
ArthurMacielOliveira <arthur_oliveira1@estudante.sc.senai.br>
2022-07-11 22:28:52 -0300

    .

--- app/src/main/java/com/example/myapplication/MenuActivity.java
+++ app/src/main/java/com/example/myapplication/MenuActivity.java
@@ -8,22 +8,43 @@ import android.widget.ImageView;
 
 public class MenuActivity extends Activity {
     ImageView switchToHomerActivity;
+    ImageView switchToHomerrActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        switchToHomerActivity = (ImageView) findViewById(R.id.imageView3);
+        switchToHomerActivity = (ImageView) findViewById(R.id.imageView4);
+        switchToHomerrActivity = (ImageView) findViewById(R.id.imageView3);
+
         switchToHomerActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 switchActivities();
             }
         });
+        switchToHomerrActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivitie();
+            }
+        });
     }
-    private void switchActivities(){
+//        switchToHomerActivity = (ImageView) findViewById(R.id.imageView4);
+//        switchToHomerActivity.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                switchActivities();
+//            }
+//        });
+
+
+
+    private void switchActivities() {
         Intent switchActivityIntent = new Intent(this, HomerActivity.class);
         startActivity(switchActivityIntent);
-
     }
-}
+    private void switchActivitie(){
+        Intent switchActivityIntent = new Intent(this, HomerrActivity.class);
+        startActivity(switchActivityIntent);
+}}
